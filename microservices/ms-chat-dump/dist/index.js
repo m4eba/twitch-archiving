eda32c4196e48b451097536548baf0aea1d35b7b
m4eba <m4eba.0@gmail.com>
2022-04-19 04:52:50 +0200

    Add chat dump

---
+++ microservices/ms-chat-dump/dist/index.js
@@ -0,0 +1,46 @@
+import { KafkaConfigOpt, FileConfigOpt, } from '@twitch-archiving/config';
+import path from 'path';
+import pino from 'pino';
+import { Kafka } from 'kafkajs';
+import { parse } from 'ts-command-line-args';
+import { FileWriter } from '@twitch-archiving/utils';
+const DumpConfigOpt = {
+    topic: { type: String },
+    path: { type: String },
+};
+const config = parse({
+    ...KafkaConfigOpt,
+    ...DumpConfigOpt,
+    ...FileConfigOpt,
+}, {
+    loadFromFileArg: 'config',
+});
+const logger = pino({ level: 'debug' }).child({
+    module: 'websocket-dump',
+});
+const kafka = new Kafka({
+    clientId: config.kafkaClientId,
+    brokers: config.kafkaBroker,
+});
+logger.info({ topic: config.topic }, 'subscribe');
+const consumer = kafka.consumer({ groupId: 'websocket-dump' });
+await consumer.connect();
+await consumer.subscribe({ topic: config.topic, fromBeginning: true });
+const out = new Map();
+await consumer.run({
+    eachMessage: async ({ message }) => {
+        if (message.value && message.key) {
+            logger.info({
+                message: JSON.parse(message.value.toString()),
+            }, 'msg received');
+            const key = message.key.toString();
+            let o = out.get(key);
+            if (o === undefined) {
+                o = new FileWriter(path.join(config.path, key));
+                out.set(key, o);
+            }
+            const irc = JSON.parse(message.value.toString());
+            o.write(new Date(parseInt(message.timestamp)).toISOString(), irc.raw);
+        }
+    },
+});
