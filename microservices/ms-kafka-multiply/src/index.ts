2598f0ec8f2ff4e96c57d0575ebbd979fbcbe076
m4eba <m4eba.0@gmail.com>
2022-04-19 13:37:39 +0200

    Add kafka multiplier

---
+++ microservices/ms-kafka-multiply/src/index.ts
@@ -0,0 +1,64 @@
+import {
+  KafkaConfig,
+  KafkaConfigOpt,
+  FileConfig,
+  FileConfigOpt,
+} from '@twitch-archiving/config';
+import pino, { Logger } from 'pino';
+import { Kafka, Consumer, Producer, TopicMessages } from 'kafkajs';
+import { ArgumentConfig, parse } from 'ts-command-line-args';
+
+interface MultiConfig {
+  inputTopic: string;
+  outputTopic: string[];
+}
+
+const MultiConfigOpt: ArgumentConfig<MultiConfig> = {
+  inputTopic: { type: String },
+  outputTopic: { type: String, multiple: true },
+};
+
+interface Config extends MultiConfig, KafkaConfig, FileConfig {}
+
+const config: Config = parse<Config>(
+  {
+    ...KafkaConfigOpt,
+    ...MultiConfigOpt,
+    ...FileConfigOpt,
+  },
+  {
+    loadFromFileArg: 'config',
+  }
+);
+
+const logger: Logger = pino({ level: 'debug' }).child({
+  module: 'kafka-multi',
+});
+
+const kafka: Kafka = new Kafka({
+  clientId: config.kafkaClientId,
+  brokers: config.kafkaBroker,
+});
+
+logger.info({ topic: config.inputTopic }, 'subscribe');
+const consumer: Consumer = kafka.consumer({ groupId: 'websocket-dump' });
+await consumer.connect();
+await consumer.subscribe({ topic: config.inputTopic, fromBeginning: true });
+
+const producer: Producer = kafka.producer();
+await producer.connect();
+
+// TODO use eachBatch instead
+await consumer.run({
+  eachMessage: async ({ message }) => {
+    const messages: TopicMessages[] = [];
+    for (let i: number = 0; i < config.outputTopic.length; ++i) {
+      const topicMessage: TopicMessages = {
+        topic: config.outputTopic[i],
+        messages: [message],
+      };
+      messages.push(topicMessage);
+    }
+    await producer.sendBatch({ topicMessages: messages });
+  },
+});
