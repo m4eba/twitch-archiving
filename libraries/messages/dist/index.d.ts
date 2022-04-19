c41499812db7151af17a82242b8f211cb87e131e
m4eba <m4eba.0@gmail.com>
2022-04-19 02:07:22 +0200

    Add raw member variable to IRCMessage

--- libraries/messages/dist/index.d.ts
+++ libraries/messages/dist/index.d.ts
@@ -1,3 +1,10 @@
 export interface WebsocketMessage {
     data: string;
 }
+export interface IRCMessage {
+    raw: string;
+    tags: Record<string, string>;
+    prefix: string;
+    command: string;
+    params: string[];
+}
--- libraries/messages/src/index.ts
+++ libraries/messages/src/index.ts
@@ -1,3 +1,11 @@
 export interface WebsocketMessage {
   data: string;
 }
+
+export interface IRCMessage {
+  raw: string;
+  tags: Record<string, string>;
+  prefix: string;
+  command: string;
+  params: string[];
+}
