58e06da21e20a11b24ac8b3055b946c1ffb2e593
m4eba <m4eba.0@gmail.com>
2022-04-29 13:15:34 +0200

    Add pubsub filter microservice

---
+++ microservices/ms-pubsub-filter/dist/index.js
@@ -0,0 +1,54 @@
+import { KafkaConfigOpt, FileConfigOpt, } from '@twitch-archiving/config';
+import pino from 'pino';
+import { Kafka } from 'kafkajs';
+import { parse } from 'ts-command-line-args';
+import fs from 'fs';
+const PubsubFilterConfigOpt = {
+    inputTopic: { type: String },
+    filterFile: { type: String },
+};
+const config = parse({
+    ...KafkaConfigOpt,
+    ...PubsubFilterConfigOpt,
+    ...FileConfigOpt,
+}, {
+    loadFromFileArg: 'config',
+});
+const logger = pino({ level: 'debug' }).child({
+    module: 'pubsub-filter',
+});
+const kafka = new Kafka({
+    clientId: config.kafkaClientId,
+    brokers: config.kafkaBroker,
+});
+logger.info({ topic: config.inputTopic }, 'subscribe');
+const consumer = kafka.consumer({ groupId: 'websocket-dump' });
+await consumer.connect();
+await consumer.subscribe({ topic: config.inputTopic, fromBeginning: true });
+const producer = kafka.producer();
+await producer.connect();
+const filter = JSON.parse(await fs.promises.readFile(config.filterFile, { encoding: 'utf8' }));
+await consumer.run({
+    eachMessage: async ({ message }) => {
+        if (message.value) {
+            const event = JSON.parse(message.value.toString());
+            for (const key in filter) {
+                if (event.topic.startsWith(key)) {
+                    await sendData(filter[key], message);
+                }
+            }
+        }
+    },
+});
+async function sendData(topic, msg) {
+    const messages = [];
+    for (let i = 0; i < topic.length; ++i) {
+        const topicMessage = {
+            topic: topic[i],
+            messages: [msg],
+        };
+        messages.push(topicMessage);
+    }
+    logger.debug({ topic: topic, size: messages.length }, 'sending batch');
+    await producer.sendBatch({ topicMessages: messages });
+}
---
+++ microservices/ms-pubsub-filter/src/index.ts
@@ -0,0 +1,83 @@
+import {
+  KafkaConfig,
+  KafkaConfigOpt,
+  FileConfig,
+  FileConfigOpt,
+} from '@twitch-archiving/config';
+import pino, { Logger } from 'pino';
+import { Kafka, Producer, Consumer, TopicMessages, Message } from 'kafkajs';
+import { ArgumentConfig, parse } from 'ts-command-line-args';
+import fs from 'fs';
+
+interface PubsubFilterFile {
+  [key: string]: string[];
+}
+
+interface PubsubFilterConfig {
+  inputTopic: string;
+  filterFile: string;
+}
+
+const PubsubFilterConfigOpt: ArgumentConfig<PubsubFilterConfig> = {
+  inputTopic: { type: String },
+  filterFile: { type: String },
+};
+
+interface Config extends PubsubFilterConfig, KafkaConfig, FileConfig {}
+
+const config: Config = parse<Config>(
+  {
+    ...KafkaConfigOpt,
+    ...PubsubFilterConfigOpt,
+    ...FileConfigOpt,
+  },
+  {
+    loadFromFileArg: 'config',
+  }
+);
+
+const logger: Logger = pino({ level: 'debug' }).child({
+  module: 'pubsub-filter',
+});
+
+const kafka: Kafka = new Kafka({
+  clientId: config.kafkaClientId,
+  brokers: config.kafkaBroker,
+});
+logger.info({ topic: config.inputTopic }, 'subscribe');
+const consumer: Consumer = kafka.consumer({ groupId: 'websocket-dump' });
+await consumer.connect();
+await consumer.subscribe({ topic: config.inputTopic, fromBeginning: true });
+
+const producer: Producer = kafka.producer();
+await producer.connect();
+
+const filter: PubsubFilterFile = JSON.parse(
+  await fs.promises.readFile(config.filterFile, { encoding: 'utf8' })
+);
+
+await consumer.run({
+  eachMessage: async ({ message }) => {
+    if (message.value) {
+      const event: { topic: string } = JSON.parse(message.value.toString());
+      for (const key in filter) {
+        if (event.topic.startsWith(key)) {
+          await sendData(filter[key], message);
+        }
+      }
+    }
+  },
+});
+
+async function sendData(topic: string[], msg: Message): Promise<void> {
+  const messages: TopicMessages[] = [];
+  for (let i: number = 0; i < topic.length; ++i) {
+    const topicMessage: TopicMessages = {
+      topic: topic[i],
+      messages: [msg],
+    };
+    messages.push(topicMessage);
+  }
+  logger.debug({ topic: topic, size: messages.length }, 'sending batch');
+  await producer.sendBatch({ topicMessages: messages });
+}
