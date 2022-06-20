a72eaf06525a68687e0040351e9cf683fed1b404
m4eba <m4eba.0@gmail.com>
2022-06-20 12:29:18 +0200

    Add timeout to call to text()

--- libraries/utils/dist/fetch.d.ts
+++ libraries/utils/dist/fetch.d.ts
@@ -1,2 +1,3 @@
 import type { Response } from 'node-fetch';
+export declare function fetchWithTimeoutText(url: string, retries?: number, timeout?: number): Promise<string>;
 export declare function fetchWithTimeout(url: string, retries?: number, timeout?: number): Promise<Response>;
--- libraries/utils/dist/fetch.js
+++ libraries/utils/dist/fetch.js
@@ -1,5 +1,42 @@
 import fetch from 'node-fetch';
 import AbortController from 'abort-controller';
+export async function fetchWithTimeoutText(url, retries = 5, timeout = 5000) {
+    return new Promise((resolve, reject) => {
+        const abort = new AbortController();
+        const timer = setTimeout(() => {
+            abort.abort();
+        }, timeout);
+        fetch(url, {
+            signal: abort.signal,
+        })
+            .then((resp) => {
+            resp
+                .text()
+                .then(resolve)
+                .catch((e) => {
+                if (retries === 0) {
+                    reject(e);
+                    return;
+                }
+                fetchWithTimeoutText(url, retries - 1, timeout)
+                    .then(resolve)
+                    .catch(reject);
+            });
+        })
+            .catch((e) => {
+            if (retries === 0) {
+                reject(e);
+                return;
+            }
+            fetchWithTimeoutText(url, retries - 1, timeout)
+                .then(resolve)
+                .catch(reject);
+        })
+            .finally(() => {
+            clearTimeout(timer);
+        });
+    });
+}
 export async function fetchWithTimeout(url, retries = 5, timeout = 5000) {
     return new Promise((resolve, reject) => {
         const abort = new AbortController();
