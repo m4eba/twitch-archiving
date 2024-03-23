bb552e146e38a8d6c7721ce5333f9aed98fb03b2
m4eba <m4eba.0@gmail.com>
2024-03-23 10:29:51 +0100

    [lib] ffprobe method

--- libraries/utils/src/ffmpeg.ts
+++ libraries/utils/src/ffmpeg.ts
@@ -1,6 +1,8 @@
 import type { Logger } from 'pino';
 import type { FfmpegCommand } from 'fluent-ffmpeg';
 import { initLogger } from './logger.js';
+import { promisify } from 'util';
+import { exec } from 'child_process';
 
 const logger: Logger = initLogger('ffmpeg-utils');
 
@@ -18,4 +20,58 @@ export async function execFfmpeg(command: FfmpegCommand): Promise<string> {
     command.run();
   });
   return p;
+}
+
+const pexec = promisify(exec);
+
+export interface VideoProbe {
+  streams: VideoProbeStream[];
+  format: {
+    filename: string;
+    start_time: string;
+    duration: string;
+    format_name: string;
+    size: string;
+  };
+}
+
+export interface VideoProbeStream {
+  index: number;
+  codec_name: string;
+  codec_type: string;
+  codec_tag_string: string;
+  codec_tag: string;
+  width: number;
+  height: number;
+  codec_width: number;
+  codec_height: number;
+  time_base: string;
+  duration?: string;
+  duration_ts: number;
+  r_frame_rate: string;
+  avg_frame_rate: string;
+  nb_frames?: string;
+}
+
+export async function ffprobe(filename: string): Promise<VideoProbe> {
+  const probec = `ffprobe -v quiet \
+    -print_format json -show_format \
+    -show_streams ${filename}`;
+
+  const { stdout } = await pexec(probec);
+  const probe = JSON.parse(stdout);
+
+  return probe;
+}
+
+export function getVideoProbeStream(
+  probe: VideoProbe
+): VideoProbeStream | null {
+  for (let i = 0; i < probe.streams.length; ++i) {
+    const s = probe.streams[i];
+    if (s.codec_type === 'video') {
+      return s;
+    }
+  }
+  return null;
 }
